ddf5654d018277319a9a7a2717ecc026244da3c8
Jorge <jepers2021@gmail.com>
2021-04-03 09:44:56 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace A133590.Ejercicio36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 36");
+            Console.WriteLine("Programa que recibe ingresos hasta que el mismo sea una fecha válida.");
+            DateTime ingreso;
+            Console.Write("Por favor, ingrese una fecha: ");
+            while (!DateTime.TryParse(Console.ReadLine(), out ingreso))
+            {
+                Console.Write("Ingreso inválido, intente nuevamente: ");
+            }
+            Console.WriteLine("Ingreso válido.")
+            Console.WriteLine("Presione cualquier tecla para continuar..");
+            Console.ReadKey();
+        }
+    }
+}
